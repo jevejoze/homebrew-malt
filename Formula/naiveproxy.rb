c0aff375a4bec123a8d9fba5365b7f1cac012e99
simplejava8 <simplejava8@qq.com>
2021-09-03 11:21:52 +0800

    Update naiveproxy.rb

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -4,7 +4,7 @@ class Naiveproxy < Formula
   version "93.0.4577.63-1"
   if Hardware::CPU.intel?
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64.tar.xz"
-    sha256 "eb69ced497cc62c545e18ebfaced2f7172aabb2a8d81b3cfa30b897766f98f7e"
+    sha256 "059c69f126d50bb45813eb677a7da5ddfde1b4c14baabaa44536ed7a7f15c009"
   else
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64.tar.xz"
     sha256 "d2fc5d857c819fc2832a0d9b866e1da92becb20cbafa0af1947316896edfc067"
