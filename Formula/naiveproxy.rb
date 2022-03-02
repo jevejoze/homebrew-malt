0b7f188f10a497773d4b20e9ccc43c5ce8ae7d7c
simplejava8 <simplejava8@qq.com>
2022-03-02 20:52:16 +0800

    update naiveproxy to v98.0.4758.80-2

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,13 +1,13 @@
 class Naiveproxy < Formula
   desc "Network proxy"
   homepage "https://github.com/klzgrad/naiveproxy"
-  version "98.0.4758.80-1"
+  version "v98.0.4758.80-2"
   if Hardware::CPU.intel?
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64.tar.xz"
-    sha256 "2e8513de17cfe188137f43dac4543fd1db20ee5e9ec1d88393a918d3c61f7e11"
+    sha256 "84b5b9578dda0eb5c1083417384264203a02370f5251ea0620aae4aa6be0dfdb"
   else
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64.tar.xz"
-    sha256 "618c277d89a7429a4b35210707ba426e1a59dfe52d7390299f22647738d0322d"
+    sha256 "12e7db4f1be0b3f015700a523efa3a0540f8fb74094e7cb095063a142e32b2fc"
   end
   license "BSD-3-Clause"
 
