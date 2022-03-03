7de7fb63cac94dd17dafc0b8ec02470505a6563f
github action <>
2022-03-03 06:23:46 +0000

    update naiveproxy to 99.0.4844.51-1

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,19 +1,18 @@
 class Naiveproxy < Formula
   desc "Network proxy"
   homepage "https://github.com/klzgrad/naiveproxy"
-  version "98.0.4758.80-2"
+  version "99.0.4844.51-1"
   if Hardware::CPU.intel?
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64.tar.xz"
-    sha256 "f11246b673053f21ab156e4eaab875f4560a830636e6c73a9e8ca3ed04e234ba"
+    sha256 "7c1352295ebedb270b2fd0c123ecd18ef2495a9e7b2ca3d0641e77dd94c8cf2f"
   else
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64.tar.xz"
-    sha256 "447b157e10022022325b9383bcfb48c53100707a66c94598a4003769e70c8fca"
+    sha256 "c3e325cd0b02f5367854b5a416b32c411f93e93a6a6ba8f0d2ef32a1d95216ae"
   end
   license "BSD-3-Clause"
 
   def install
     bin.install "naive"
-
     pkgetc.install "config.json"
   end
   
