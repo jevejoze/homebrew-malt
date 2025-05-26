0dd8f9a58fe72ed33737e0f0ff8b3f852df4ef06
github action <>
2025-05-26 01:14:11 +0000

    update naiveproxy to 136.0.7103.44-2

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,13 +1,13 @@
 class Naiveproxy < Formula
   desc "Network proxy"
   homepage "https://github.com/klzgrad/naiveproxy"
-  version "136.0.7103.44-1"
+  version "136.0.7103.44-2"
   if Hardware::CPU.intel?
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64.tar.xz"
-    sha256 "cbd6dbeaad1e65ba8ac15cc5e436b5a656f8d3782727a82a5570c875128befcb"
+    sha256 "e43807b63caa6ecac711f10284951dd3b23785f2fb9facbabc55ce143059462b"
   else
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64.tar.xz"
-    sha256 "ff17bdf3298f02246ae07f95bb8a66392fd511b075ffd01c887d4ae8cb1a3802"
+    sha256 "f38d562f890bb3cf0f9a77e85b233fc7d00cc235056b82e559ac850e9936580e"
   end
   license "BSD-3-Clause"
 
