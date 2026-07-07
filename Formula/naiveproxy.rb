9429d3e38bff8491f75767718555ed65f96fb703
jevejoze <jevejoze@gmail.com>
2026-07-07 12:09:06 +0800

    Update sha256 checksums for naiveproxy

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -4,10 +4,10 @@ class Naiveproxy < Formula
   version "150.0.7871.63-1"
   if Hardware::CPU.intel?
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-x64-x64.tar.xz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "92e7fe5f3cfca5e0cca49798e5c435a5bd7ef05b543c0aa614a5623ad39a50d4"
   else
     url "https://github.com/klzgrad/naiveproxy/releases/download/v#{version}/naiveproxy-v#{version}-mac-arm64-arm64.tar.xz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "315f946fa91a65a30b25b69bba88836a117838f58b616c6a58c970c9ff1d9bbf"
   end
   license "BSD-3-Clause"
 
